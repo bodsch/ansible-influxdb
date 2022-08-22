11e97aaf6ee9e8bd08bf50ade97f6e8114475190
Bodo Schulz <bodo@boone-schulz.de>
2022-08-22 06:25:10 +0200

    add tests

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,122 @@
+
+from ansible.parsing.dataloader import DataLoader
+from ansible.template import Templar
+import pytest
+import os
+import testinfra.utils.ansible_runner
+
+import pprint
+pp = pprint.PrettyPrinter()
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def base_directory():
+    cwd = os.getcwd()
+
+    if 'group_vars' in os.listdir(cwd):
+        directory = "../.."
+        molecule_directory = "."
+    else:
+        directory = "."
+        molecule_directory = f"molecule/{os.environ.get('MOLECULE_SCENARIO_NAME')}"
+
+    return directory, molecule_directory
+
+
+def read_ansible_yaml(file_name, role_name):
+    """
+    """
+    read_file = None
+
+    for e in ["yml", "yaml"]:
+        test_file = f"{file_name}.{e}"
+        if os.path.isfile(test_file):
+            read_file = test_file
+            break
+
+    return f"file={read_file} name={role_name}"
+
+@pytest.fixture()
+def get_vars(host):
+    """
+    """
+    base_dir, molecule_dir = base_directory()
+    distribution = host.system_info.distribution
+    operation_system = None
+
+    if distribution in ['debian', 'ubuntu']:
+        operation_system = "debian"
+    elif distribution in ['redhat', 'ol', 'centos', 'rocky', 'almalinux']:
+        operation_system = "redhat"
+    elif distribution in ['arch', 'artix']:
+        operation_system = f"{distribution}linux"
+
+    file_defaults      = read_ansible_yaml(f"{base_dir}/defaults/main", "role_defaults")
+    file_vars          = read_ansible_yaml(f"{base_dir}/vars/main", "role_vars")
+    file_distibution   = read_ansible_yaml(f"{base_dir}/vars/{operation_system}", "role_distibution")
+    file_molecule      = read_ansible_yaml(f"{molecule_dir}/group_vars/all/vars", "test_vars")
+    # file_host_molecule = read_ansible_yaml(f"{base_dir}/host_vars/{HOST}/vars", "host_vars")
+
+    defaults_vars = host.ansible("include_vars", file_defaults).get("ansible_facts").get("role_defaults")
+    vars_vars = host.ansible("include_vars", file_vars).get("ansible_facts").get("role_vars")
+    distibution_vars = host.ansible("include_vars", file_distibution).get("ansible_facts").get("role_distibution")
+    molecule_vars = host.ansible("include_vars", file_molecule).get("ansible_facts").get("test_vars")
+
+    ansible_vars = defaults_vars
+    ansible_vars.update(vars_vars)
+    ansible_vars.update(distibution_vars)
+    ansible_vars.update(molecule_vars)
+
+    templar = Templar(loader=DataLoader(), variables=ansible_vars)
+    result = templar.template(ansible_vars, fail_on_undefined=False)
+
+    return result
+
+
+@pytest.mark.parametrize("directories", [
+    "/etc/influxdb",
+    "/var/lib/influxdb",
+    "/var/lib/influxdb/wal",
+    "/var/lib/influxdb/data",
+    "/var/lib/influxdb/meta",
+])
+def test_directories(host, directories):
+
+    d = host.file(directories)
+    assert d.is_directory
+
+
+@pytest.mark.parametrize("files", [
+    "/etc/influxdb/config.yml",
+    "/etc/default/influxdb"
+])
+def test_files(host, files):
+    f = host.file(files)
+    assert f.is_file
+
+
+def test_service_running_and_enabled(host):
+    service = host.service('influxdb')
+    assert service.is_running
+    assert service.is_enabled
+
+
+def test_listening_socket(host, get_vars):
+    """
+    """
+    listening = host.socket.get_listening_sockets()
+
+    for i in listening:
+        print(i)
+
+    bind_address = "127.0.0.1"
+    bind_port = 8086
+
+    listen = []
+    listen.append(f"tcp://{bind_address}:{bind_port}")
+
+    for spec in listen:
+        socket = host.socket(spec)
+        assert socket.is_listening
